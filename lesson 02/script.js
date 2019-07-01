762aba0e6c17b1ea39c943fb0acde55ee77be5d6
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-07-01 22:25:39 +0300

    Исправил урок №2

--- lesson 02/script.js
+++ lesson 02/script.js
@@ -1,15 +1,15 @@
 let money = 15525,
     income = 'фриланс',
-    addExpenses = ['прогулка', 'комунальные услуги', 'уроки JS', 'уроки OpenCart'],
+    addExpenses = 'прогулка, комунальные услуги, уроки JS, уроки OpenCart',
     deposit = true,
     mission = 90000000,
     period = 10,
-    budgetDay = 150000;
+    budgetDay = money / 30;
 console.log(typeof money);
 console.log(typeof income);
 console.log(income.length);
-console.log(addExpenses);
+console.log(addExpenses.split(', '));
 console.log(typeof deposit);
 console.log(mission);
 console.log(period);
-console.log(budgetDay / 30);
+console.log(budgetDay);
