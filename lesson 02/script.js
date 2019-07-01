834c826972ea8ca6d577c54b7f8e4fbeded545a9
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-07-01 22:02:39 +0300

    Урок второй - обязательный

--- lesson 02/script.js
+++ lesson 02/script.js
@@ -1,7 +1,15 @@
-let money = prompt('Доход за месяц: ', '');
-    // income,
-    // addExpenses,
-    // deposit,
-    // mission,
-    // period;
-console.log('Доход за месяц: ' + money);
+let money = 15525,
+    income = 'фриланс',
+    addExpenses = ['прогулка', 'комунальные услуги', 'уроки JS', 'уроки OpenCart'],
+    deposit = true,
+    mission = 90000000,
+    period = 10,
+    budgetDay = 150000;
+console.log(typeof money);
+console.log(typeof income);
+console.log(income.length);
+console.log(addExpenses);
+console.log(typeof deposit);
+console.log(mission);
+console.log(period);
+console.log(budgetDay / 30);
