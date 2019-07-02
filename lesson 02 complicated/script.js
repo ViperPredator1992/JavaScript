2abb976b2f9b3a40ea4af5877a7fa2634c57932f
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-07-02 11:08:15 +0300

    вывел первые два числа

--- lesson 02 complicated/script.js
+++ lesson 02 complicated/script.js
@@ -1,4 +1,4 @@
 let num = 266219;
 console.log(num = 2 * 6 * 6 * 2 * 1 * 9);
-console.log(num = num ** 3);
-console.log(num.substr(0,2));
+console.log(num **= 3);
+console.log(String(num).slice(0, 2));
