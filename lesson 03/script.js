f3058ac55db44b021c4f75c7758d179f39f75161
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-07-04 22:54:17 +0300

    Второй комит

--- lesson 03/script.js
+++ lesson 03/script.js
@@ -1,32 +1,32 @@
 'use strict';
 
 let money = prompt('Ваш месячный доход?', ''),
-    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', ''),
-    deposit = confirm('Есть ли у вас депозит в банке?'),
+    // addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', ''),
+    // deposit = confirm('Есть ли у вас депозит в банке?'),
     requiredMonthlyExpenses1 = prompt('Какие обязательные ежемесячные расходы у вас есть?', ''),
-    requiredMonthlyExpenses2 = prompt('Какие обязательные ежемесячные расходы у вас есть?', ''),
     howMuchCost1 = prompt('Во сколько это обойдется?', ''),
+    requiredMonthlyExpenses2 = prompt('Какие обязательные ежемесячные расходы у вас есть?', ''),
     howMuchCost2 = prompt('Во сколько это обойдется?', ''),
     budgetMonth = money - howMuchCost1 - howMuchCost2,
-    mission = 100,
+    mission = 65400,
     target = mission / budgetMonth,
     budgetDay = budgetMonth / 30;
 
     if (budgetDay >= 800) {
         console.log('Высокий уровень дохода');
     }
-    else if (budgetDay >= 300 && budgetDay <= 800) {
+    else if (budgetDay >= 300 && budgetDay < 800) {
         console.log('Средний уровень дохода');
     }
-    else if (budgetDay > 0 && budgetDay < 300) {
+    else if (budgetDay >= 0 && budgetDay < 300) {
         console.log('Низкий уровень дохода');
     } else {
         console.log('Что то пошло не так');
     }
 
 console.log(money);
-console.log(addExpenses.split(', '));
-console.log(deposit);
+// console.log(addExpenses.split(', '));
+// console.log(deposit);
 console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
