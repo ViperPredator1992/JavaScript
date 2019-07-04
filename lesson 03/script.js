3433a22fd9a534f9cb976b45923a8a180ad51e29
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-07-04 15:48:58 +0300

    --- Auto-Git Commit ---

--- lesson 03/script.js
+++ lesson 03/script.js
@@ -1,12 +1,11 @@
 'use strict';
 
 let money = prompt('Ваш месячный доход?', ''),
-    addExpenses = [],
-    arr = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', '');
+    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', ''),
+    deposit = con
+    
 
-   // arr ;
-    addExpenses = arr;
 console.log(money);
-console.log(addExpenses);
+console.log(addExpenses.split(', '));
 
 
