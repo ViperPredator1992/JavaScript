1a2cd67afc0de9f8abfeceeeaca49eb641240f99
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-07-04 13:35:56 +0300

    --- Auto-Git Commit ---

---
+++ lesson 03/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let money = 155255,
+    income = 'фриланс',
+    addExpenses = 'прогулка, комунальные услуги, уроки JS, уроки OpenCart',
+    deposit = true,
+    mission = 90000000,
+    period = 10,
+    budgetDay = money / 30;
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(income.length);
+console.log(addExpenses.split(', '));
+console.log(mission);
+console.log(period);
+console.log(budgetDay);
+console.log(money % 30);
