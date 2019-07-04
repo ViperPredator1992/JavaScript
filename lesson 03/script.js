b4a6cde43dccaabee527df77521e5ddf50596fac
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-07-04 19:08:28 +0300

    --- Auto-Git Commit ---

--- lesson 03/script.js
+++ lesson 03/script.js
@@ -2,17 +2,20 @@
 
 let money = prompt('Ваш месячный доход?', ''),
     //addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', ''),
-    //deposit = confirm('Есть ли у вас депозит в банке?'),
-    requiredMonthlyExpenses1 = prompt('Какие обязательные ежемесячные расходы у вас есть?', ''),
-    requiredMonthlyExpenses2 = prompt('Какие обязательные ежемесячные расходы у вас есть?', ''),
+    deposit = confirm('Есть ли у вас депозит в банке?');
+    // requiredMonthlyExpenses1 = prompt('Какие обязательные ежемесячные расходы у вас есть?', ''),
+    // requiredMonthlyExpenses2 = prompt('Какие обязательные ежемесячные расходы у вас есть?', ''),
     //howMuchCost1 = prompt('Во сколько это обойдется?', ''),
     //howMuchCost2 = prompt('Во сколько это обойдется?', ''),
-    budgetMonth = money - requiredMonthlyExpenses1;
-    budgetMonth = money - requiredMonthlyExpenses2;
+    // budgetMonth = money - requiredMonthlyExpenses1;
+    // budgetMonth = money - requiredMonthlyExpenses2;
     
+    if (true) {console.log('Да');}
+    else {console.log('Нет');}
+
 console.log(money);
 // console.log(addExpenses.split(', '));
-// console.log(deposit);
+console.log(deposit);
 // console.log(typeof money);
 // console.log(typeof income);
 // console.log(typeof deposit);
@@ -20,5 +23,5 @@ console.log(money);
 // console.log(requiredMonthlyExpenses2);
 // console.log(howMuchCost1);
 // console.log(howMuchCost2);
-console.log(budgetMonth);
+//console.log(budgetMonth);
 
