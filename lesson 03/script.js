ab09d9685a1941e557b6b52d35c03f76d6dc219d
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-07-04 16:19:05 +0300

    --- Auto-Git Commit ---

--- lesson 03/script.js
+++ lesson 03/script.js
@@ -2,10 +2,19 @@
 
 let money = prompt('Ваш месячный доход?', ''),
     addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', ''),
-    deposit = con
+    deposit = confirm('Есть ли у вас депозит в банке?'),
+    requiredMonthlyExpenses1 = prompt('Какие обязательные ежемесячные расходы у вас есть?', ''),
+    requiredMonthlyExpenses2 = prompt('Какие обязательные ежемесячные расходы у вас есть?', ''),
+    howMuchCost1 = prompt('Во сколько это обойдется?', ''),
+    howMuchCost2 = prompt('Во сколько это обойдется?', '');
     
-
 console.log(money);
 console.log(addExpenses.split(', '));
-
-
+console.log(deposit);
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(requiredMonthlyExpenses1);
+console.log(requiredMonthlyExpenses2);
+console.log(howMuchCost1);
+console.log(howMuchCost2);
