86d1e59a9298ff30fae6492f35e61038b8ac28a9
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-07-06 01:05:15 +0300

    --- Auto-Git Commit ---

--- lesson 03 complicated/script.js
+++ lesson 03 complicated/script.js
@@ -5,36 +5,36 @@
 // Написать условия при котором в зависимости от значения lang будут выводится дни недели на русском или английском языке.
 // Решите задачу через if, switch, через многомерный массив без ифов и switch.
 
-let lang = prompt('Введите день недели на русском или английском языке', ''),
-    ruDay = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Вокресенье'],
-    enDay = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+// let lang = prompt('Введите день недели на русском или английском языке', ''),
+//     ruDay = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Вокресенье'],
+//     enDay = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 
-    if (lang == 'ru') {
-        console.log(ruDay);
-    } else {
-        console.log(enDay);
-    }
+//     if (lang == 'ru') {
+//         console.log(ruDay);
+//     } else {
+//         console.log(enDay);
+//     }
 
-    switch (lang) {
-        case 'ru':
-            ruDay = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Вокресенье'];
-            console.log(ruDay);
-            break;
-        case 'en':
-            enDay = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-            console.log(enDay);
-            break;
-    }
+//     switch (lang) {
+//         case 'ru':
+//             ruDay = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Вокресенье'];
+//             console.log(ruDay);
+//             break;
+//         case 'en':
+//             enDay = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+//             console.log(enDay);
+//             break;
+//     }
 
-    let languages = {
-        'ru': ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Вокресенье'],
-        'en': ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    };
-    if (lang == 'ru') {
-        console.log(languages.ru);
-    } else {
-        console.log(languages.en);
-    }
+//     let languages = {
+//         'ru': ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Вокресенье'],
+//         'en': ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+//     };
+//     if (lang == 'ru') {
+//         console.log(languages.ru);
+//     } else {
+//         console.log(languages.en);
+//     }
     
 
 //Задача №2
@@ -42,4 +42,11 @@ let lang = prompt('Введите день недели на русском ил
 // если значение“ Максим” то вывести в консоль“ преподаватель”, с любым другим значением вывести в консоль“ студент”
 // Решить задачу с помощью нескольких тернарных операторов, без использования if или switch
 
-let namePerson = 'Артем' ? 'директор' Ж;
+let namePerson = prompt('Введите имя', '');
+
+    namePerson == 'Артем' ? 'директор' : 'студент',
+    namePerson == 'Максим' ? 'преподаватель' : 'студент',
+    namePerson == namePerson ? 'студент' : 'студент';
+//let total =
+console.log(namePerson);
+
