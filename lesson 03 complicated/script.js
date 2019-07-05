c549c8dbc4798e1045dfc5d3c3bab92057b41574
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-07-05 14:33:15 +0300

    --- Auto-Git Commit ---

--- lesson 03 complicated/script.js
+++ lesson 03 complicated/script.js
@@ -1,11 +1,13 @@
 'use strict';
 
-let lang = ['ru', 'en'],
+let lang = 'ru, en',
     russian = prompt('Введите день недели на русском или английском языке', '');
-if (lang[0]) {
-    console.log(lang[0]);
+
     
-} else if (lang[1]) {
-    console.log(lang[1]);
+if (lang[0] == russian) {
+    console.log(['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Вокресенье']);
+} else if (lang[1] == russian) {
+    console.log(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']);
 }
-console.log(lang);
+
+console.log(lang.split(', '));
