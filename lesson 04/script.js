0cec2cfd1d13415901c372dcc0f9aaac1d37fd10
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-07-06 15:28:41 +0300

    function

--- lesson 04/script.js
+++ lesson 04/script.js
@@ -1,11 +1,11 @@
 'use strict';
 
 let money = +prompt('Ваш месячный доход?', ''),
-    //addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', ''),
-    //deposit = confirm('Есть ли у вас депозит в банке?'),
-    //requiredMonthlyExpenses1 = prompt('Какие обязательные ежемесячные расходы у вас есть?', ''),
+    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', ''),
+    deposit = confirm('Есть ли у вас депозит в банке?'),
+    requiredMonthlyExpenses1 = prompt('Какие обязательные ежемесячные расходы у вас есть?', ''),
     howMuchCost1 = +prompt('Во сколько это обойдется?', ''),
-    //requiredMonthlyExpenses2 = prompt('Какие обязательные ежемесячные расходы у вас есть?', ''),
+    requiredMonthlyExpenses2 = prompt('Какие обязательные ежемесячные расходы у вас есть?', ''),
     howMuchCost2 = +prompt('Во сколько это обойдется?', ''),
     income = 'Фриланс',
     budgetMonth = money - howMuchCost1 - howMuchCost2,
@@ -41,15 +41,15 @@ console.log(getTargetMonth());
 
 
 console.log(money);
-// console.log(addExpenses.split(', '));
-// console.log(deposit);
-// console.log(typeof money);
-// console.log(typeof income);
-// console.log(typeof deposit);
-// console.log(requiredMonthlyExpenses1);
-// console.log(requiredMonthlyExpenses2);
-// console.log(howMuchCost1);
-// console.log(howMuchCost2);
-//console.log(budgetMonth);
-// console.log(Math.ceil(target));
-// console.log(Math.floor(budgetDay));
+console.log(addExpenses.split(', '));
+console.log(deposit);
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(requiredMonthlyExpenses1);
+console.log(requiredMonthlyExpenses2);
+console.log(howMuchCost1);
+console.log(howMuchCost2);
+console.log(budgetMonth);
+console.log(Math.ceil(target));
+console.log(Math.floor(budgetDay));
