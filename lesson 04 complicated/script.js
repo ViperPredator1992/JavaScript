a4025151247f37f063bc82d66e0c4d2509339bcf
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-07-08 11:57:00 +0300

    новые правки

--- lesson 04 complicated/script.js
+++ lesson 04 complicated/script.js
@@ -5,19 +5,33 @@
 // 2. В полученной(как аргумент) строке функция должна убрать все пробелы в начале и в конце
 // 3. Если строка более 30 знаков - то после 30 го символа часть текста скрывается и вместо них появляются три точки(...)
 
+
+let strText = prompt();
 function getArgument(str) {
 
-    str = prompt('', '');
-    str = str.trim();
-    if (typeof str != 'string') {
-        str += '';
+    strText = str.trim();
+
+    if (strText == Number(str)) {
         alert('Передана не строка');
     } else {
         console.log('Передана строка');
+        if (str.lenght > 5) {
+            str = str.slice(0, 5) + '...';
+            return str;
+            //console.log(str);
+        } else {
+            return str;
+        }
     }
-    if (str.length <= 30) return str;
-    str = str.slice(0, 30) + '...';
-    return str;
-
 }
-console.log(getArgument());
+console.log(getArgument(strText));
+
+            // if (typeof str != 'string') {
+            //     str += '';
+            //     alert('Передана не строка');
+            // } else {
+            //     console.log('Передана строка');
+            // }
+            // if (str.length <= 30) return str;
+            // str = str.slice(0, 30) + '...';
+            // return str;
