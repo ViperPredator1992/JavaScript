96478cd6fbbbf78da9c84d058abb505e5e515bf0
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-07-15 20:16:23 +0300

    chenges 06

--- lesson 06/script.js
+++ lesson 06/script.js
@@ -28,13 +28,8 @@ let appData = {
     asking: function () {
 
         let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'налоги, налоги, налоги');
-<<<<<<< HEAD
-            appData.addExpenses = addExpenses.toLowerCase().split(', ');
-            appData.deposit = confirm('Есть ли у вас депозит в банке?');
-=======
         appData.addExpenses = addExpenses.toLowerCase().split(', ');
         appData.deposit = confirm('Есть ли у вас депозит в банке?');
->>>>>>> 97260a2ec412ebad8bd5270c09f731ecbbae04ea
 
         for (let i = 0; i < 2; i++) {
 
@@ -51,7 +46,6 @@ let appData = {
 
     },
     getExpensesMonth: function () {
-<<<<<<< HEAD
         for (let key in appData.expenses) {
             appData.expensesMonth += +appData.expenses[key];
         }
@@ -64,7 +58,6 @@ let appData = {
     getTargetMonth: function () {
         let target = appData.mission / money;
         target = Math.ceil(target);
-=======
 
         for (let key in appData.expenses) {
             appData.expensesMonth += +appData.expenses[key];
@@ -83,21 +76,15 @@ let appData = {
         let target = appData.mission / money;
         target = Math.ceil(target);
 
->>>>>>> 97260a2ec412ebad8bd5270c09f731ecbbae04ea
         if (target > 0) {
             return ('Цель будет достигнута через ' + target + ' месяцев');
         } else {
             return ('Цель не будет достигнута');
         }
-<<<<<<< HEAD
-    },
-    getStatusIncome: function () {
-=======
 
     },
     getStatusIncome: function () {
 
->>>>>>> 97260a2ec412ebad8bd5270c09f731ecbbae04ea
         if (appData.budgetDay >= 800) {
             return ('Высокий');
         } else if (appData.budgetDay >= 300 && appData.budgetDay < 800) {
@@ -107,15 +94,6 @@ let appData = {
         } else if (appData.budgetDay >= 0) {
             return ('Что то пошло не так');
         }
-<<<<<<< HEAD
-    },
-    wholeAppData: function() {
-        for (let key in appData) {
-            console.log('Наша программа ' + key + ' включает в себя данные: ' + appData[key]);
-        }
-    }
-};
-=======
 
     },
     consolLog: function () {
@@ -134,23 +112,11 @@ let appData = {
     }
 };
 
->>>>>>> 97260a2ec412ebad8bd5270c09f731ecbbae04ea
 appData.asking();
 appData.getBudget();
 appData.wholeAppData();
 
 let expensesMonth = appData.getExpensesMonth(),
     targetMonth = appData.getTargetMonth(),
-<<<<<<< HEAD
-    statusIncome = appData.getStatusIncome();
-
-console.log('Месячный доход:', money);
-console.log('Сумма всех обязательных расходов:', appData.expensesMonth);
-console.log('Накопления за период:', appData.budgetMonth);
-console.log(targetMonth);
-console.log('Уровень дохода -', statusIncome);
-console.log(appData);
-=======
     statusIncome = appData.getStatusIncome(),
     consol = appData.consolLog();
->>>>>>> 97260a2ec412ebad8bd5270c09f731ecbbae04ea
