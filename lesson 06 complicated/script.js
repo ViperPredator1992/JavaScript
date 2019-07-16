e54dfc7cb08ce77af198f803dccf8889c8728011
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-07-16 13:42:16 +0300

    делаю по другому

--- lesson 06 complicated/script.js
+++ lesson 06 complicated/script.js
@@ -1,20 +1,38 @@
 'use strict';
 
+// let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+
+// for (let i = 0; i < week.length; i++) {
+
+//     let dayOff = document.createElement('div');
+//     dayOff.innerHTML = week[i];
+
+//     if (week[i] == 'Понедельник') {
+//         document.write(week[i].bold());
+//         let date = new Date(2019, 4, 3);
+//         console.log(date);
+//     }
+//     if (week[i] == 'Суббота' || week[i] == 'Воскресенье') {
+//         elt.style.cssText('font-weight: italic');
+//         //document.write(week[i].italics());
+//     }
+
+//     document.body.appendChild(dayOff);
+// }
+
+
 let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 
-for (let i = 0; i < week.length; i++) {
+for (let i = 0, len = week.length; i < len; i++) {
+    let html = week[i];
+    if (i === 0) {
+        html = html.italics(); // понедельник
+    } 
+    else if (i > 4) {
+        html = html.bold(); // выходные
+    } 
 
     let dayOff = document.createElement('div');
-    dayOff.innerHTML = week[i];
-
-    if (week[i] == 'Пятница') {
-        dayOff.classList.add('italic');
-        let date = new Date(2019, 4, 3);
-        console.log(date);
-    }
-    if (week[i] == 'Суббота' || week[i] == 'Воскресенье') {
-        dayOff.classList.add('bold');
-    }
-
+    dayOff.innerHTML = html;
     document.body.appendChild(dayOff);
 }
