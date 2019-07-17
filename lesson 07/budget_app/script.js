cb1f7fcc27e0e9134bccc5f325eb9cdcf56e66ab
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-07-17 19:26:59 +0300

    Calc

--- lesson 07/budget_app/script.js
+++ lesson 07/budget_app/script.js
@@ -1,2 +1,43 @@
 'use strict';
 
+let toCalculate = document.getElementById('start'),
+    firstPlus = document.getElementsByTagName('button'),
+    secondPlus = document.getElementsByTagName('button'),
+    toCheckbox = document.querySelector('#deposit-check'),
+    toIncome = document.querySelectorAll('.additional_income-item'),
+    budgetDay = document.querySelector('.budget_day-value'),
+    expensesMonth = document.querySelector('.expenses_month-value'),
+    additionalIncome = document.querySelector('.additional_income-value'),
+    additionalExpenses = document.querySelector('.additional_expenses-value'),
+    incomePeriod = document.querySelector('.income_period-value'),
+    targetMonth = document.querySelector('.target_month-value'),
+    salaryAmount = document.querySelector('.salary-amount'),
+    incomeTitle = document.querySelector('.income-title'),
+    incomeAmount = document.querySelector('.income-amount'),
+    expensesTitle = document.querySelector('.expenses-title'),
+    expensesAmount = document.querySelector('.expenses-amount'),
+    additionalExpensesItem = document.querySelector('.additional_expenses-item'),
+    targetAmount = document.querySelector('.target-amount'),
+    periodSelect = document.querySelector('.period-select'),
+    budgetMonth = document.querySelector('.budget_month-value');
+
+
+console.log(toCalculate);
+console.log(firstPlus);
+console.log(secondPlus);
+console.log(toCheckbox);
+console.log(toIncome);
+console.log(budgetDay);
+console.log(expensesMonth);
+console.log(additionalIncome);
+console.log(additionalExpenses);
+console.log(incomePeriod);
+console.log(targetMonth);
+console.log(salaryAmount);
+console.log(incomeTitle);
+console.log(expensesTitle);
+console.log(expensesAmount);
+console.log(additionalExpensesItem);
+console.log(targetAmount);
+console.log(periodSelect);
+console.log(budgetMonth);
