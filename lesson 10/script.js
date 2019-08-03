9f22feb69e29161a2ec5e7d752878175268cc08b
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-08-03 21:53:59 +0300

    универсальность с помощью `${}`

--- lesson 10/script.js
+++ lesson 10/script.js
@@ -1,26 +1,5 @@
 'use strict';
 
-/* 1) Сделать класс DomElement, который содержит свойства
-  - selector, 
-  - height, 
-  - width, 
-  - bg, 
-  - fontSize
-
-содержит метод, который создает элемент на странице
-если в селектор передана строка начинающаяся с точки ‘.’ создавать <div></div> с таким классом
-если с решетки ‘#’, то создать параграф <p></p>
-
-с помощью cssText задавать стили: 
-  - высотой - height,
-  - шириной - width, 
-  - background - bg
-  - размер текста fontSize 
-внутрь записывать любой текст
-
-2) Создать новый объект на основе класса DomElement
-
-3) Вызвать его метод чтобы получить элемент на странице */
 function DomElement(selector, height, width, backgroundColor, fontSize) {
 
     this.selector = selector;
@@ -38,35 +17,25 @@ DomElement.prototype.ride = function () {
     this.width = prompt('width', '');
     this.backgroundColor = prompt('backgroundColor', '');
     this.fontSize = prompt('fontSize', '');
+    this.text = prompt('Введите текст', '');
 
-    if (this.selector === '.') {
+    let elem;
+    if (this.selector.charAt(0) === '.') {
+        elem = document.createElement('div'); 
+        elem.className = `${this.selector}`;
+    } else if (this.selector.charAt(0) === '#') {
+        elem = document.createElement('p'); 
+        elem.id = `${this.selector}`;
+    }
 
-        let elementDiv = document.createElement('div');
-            elementDiv.className = '.';
-            elementDiv.innerHTML = prompt('Введите текст','');
-            elementDiv.style.height = this.height + 'px';
-            elementDiv.style.width = this.width + 'px';
-            elementDiv.style.backgroundColor = this.backgroundColor;
-            elementDiv.style.fontSize = this.fontSize + 'px';
-            document.body.appendChild(elementDiv);
-
-        return elementDiv;
-
-    } else if (this.selector === '#') {
-
-        let elementP = document.createElement('p');
-            elementP.className = '#';
-            elementP.innerHTML = prompt('Введите текст','');
-            elementP.style.height = this.height + 'px';
-            elementP.style.width = this.width + 'px';
-            elementP.style.backgroundColor = this.backgroundColor;
-            elementP.style.fontSize = this.fontSize + 'px';
-            document.body.appendChild(elementP);
-
-        return elementP;
-
-    }  
+    elem.innerHTML = this.text;
+    elem.style.cssText = `height: ${this.height}px; width: ${this.width}px; 
+    background: ${this.backgroundColor}; fontSize: ${this.fontSize}px`;
     
+    document.body.appendChild(elem);
+
+    return elem;
+
 };
 
 let elem = new DomElement();
