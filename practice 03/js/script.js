b2cf8c84d014aea31a1203c9070d50837f245958
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-08-18 14:14:11 +0300

    add team

--- practice 03/js/script.js
+++ practice 03/js/script.js
@@ -298,6 +298,30 @@ window.addEventListener('DOMContentLoaded', () => {
 
     slider();
 
+    // Team
+    const teamImage = () => {
+
+        const image = document.querySelectorAll('.command__photo');
+        let teamFoto;
+
+        image.forEach((elem, index) => {
+
+            image[index].addEventListener('mouseenter', (event) => {
+                teamFoto = event.target.getAttribute('src');
+                event.target.src = event.target.dataset.img;
+            });
+
+            image[index].addEventListener('mouseleave', (event) => {
+                event.target.src = teamFoto;
+            });
+
+        });
+
+
+    };
+
+    teamImage();
+
     // Calc
     const calc = (price = 100) => {
 
